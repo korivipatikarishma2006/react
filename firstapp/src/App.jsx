807d3ae95c0e59f029137bc6218307dec8ffc80b
korivipatikarishma2006 <karishmakorivipathi@gmail.com>
2026-01-31 15:05:36 +0530

    code

--- firstapp/src/App.jsx
+++ firstapp/src/App.jsx
@@ -1,6 +1,6 @@
 
 import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Home from "./components/home.jsx";
+import Home from "./components/Home.jsx";
 import Register from "./components/Register.jsx";
 import Login from "./components/Login.jsx";
 import Navigation from "./components/Navigation.jsx";
